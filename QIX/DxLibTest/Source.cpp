1006d9f63c69a02538a1e9382e9f0a7611564889
Kawano <r_kawano@asojuku.ac.jp>
2019-04-09 13:17:57 +0900

    ぱられる

--- QIX/DxLibTest/Source.cpp
+++ QIX/DxLibTest/Source.cpp
@@ -1,4 +1,6 @@
 #include<DxLib.h>
+#include<vector>
+#include<list>
 #include"Geometry.h"
 
 constexpr unsigned int scr_w = 640;
@@ -6,12 +8,17 @@ constexpr unsigned int scr_h = 480;
 constexpr unsigned int play_areaX = 50;
 constexpr unsigned int play_areaY = 50;
 
+using namespace std;
+
 int main() {
 	ChangeWindowMode(true);
 	DxLib::SetWindowText("QIX");
 	DxLib::SetGraphMode(scr_w, scr_h, 32);
 	DxLib_Init();
-	SetDrawScreen(DX_SCREEN_BACK);
+	DxLib::SetDrawScreen(DX_SCREEN_BACK);
+
+	
+
 
 	DrawString(100, 100, "Hello World", 0xffffff); 
 	Position2 playerPos(550 / 2,450);//
@@ -23,6 +30,11 @@ int main() {
 	Position2 startPos = playerPos-offset;
 	char field[225][200] = {};
 	bool firstUp = true;
+	enum class Direction {
+		up,right,down,left
+	};
+	Direction lastDirection = Direction::up;
+	std::list<Position2> keypoints;
 	while (!ProcessMessage()) {
 		ClearDrawScreen();
 		DxLib::GetHitKeyStateAll(keystate);
@@ -32,37 +44,79 @@ int main() {
 				startPos = playerPos-offset;
 				firstUp = false;
 			}
+			auto tmppos = playerPos - offset;
+			if (lastDirection == Direction::right || lastDirection == Direction::left) {
+				keypoints.push_back(tmppos);
+			}
+			lastDirection = Direction::up;
 		}else if (keystate[KEY_INPUT_DOWN]) {
 			playerPos.y  = min(play_areaY+400,playerPos.y + 2);
+			auto tmppos = playerPos - offset;
+			if (lastDirection == Direction::right || lastDirection == Direction::left) {
+				keypoints.push_back(tmppos);
+			}
+			lastDirection = Direction::down;
 		}else if (keystate[KEY_INPUT_RIGHT]) {
 			playerPos.x = min(play_areaX+450,playerPos.x + 2);
+
+			//‰E•Ç‚É“–‚½‚Á‚½Žž
 			if (playerPos.x == play_areaX + 450) {
 				//FillConvex
 				auto tmppos = playerPos - offset;
-				SetDrawScreen(area);
-				DrawBox(startPos.x, tmppos.y, tmppos.x, startPos.y, 0xaaffaa, true);
-				SetDrawScreen(DX_SCREEN_BACK);
+				DxLib::SetDrawScreen(area);
+
+				auto lastpos = startPos;
+				unsigned int maxheight=0;
+				for (auto& pos : keypoints) {
+					if (lastpos.y != pos.y) {
+						if (maxheight < abs(pos.y - lastpos.y)) {
+							DxLib::DrawBox(pos.x - 1, pos.y - 1, pos.x + 1, startPos.y + 1, 0xaaffaa, true);
+							continue;
+						}
+						maxheight = abs(pos.y - lastpos.y);
+					}
+					DxLib::DrawBox(pos.x - 1, pos.y - 1, tmppos.x + 1, lastpos.y + 1, 0xaaffaa, true);
+					lastpos = pos;
+
+				}
+				keypoints.clear();
+				firstUp = true;
+				DxLib::SetDrawScreen(DX_SCREEN_BACK);
+
 			}
+			else {
+				auto tmppos = playerPos - offset;
+				if (lastDirection == Direction::up || lastDirection == Direction::down) {
+					keypoints.push_back(tmppos);
+				}
+			}
+
+			lastDirection = Direction::right;
 		}else if (keystate[KEY_INPUT_LEFT]) {
 			playerPos.x = max(play_areaX , playerPos.x - 2);
+			auto tmppos = playerPos - offset;
+			if (lastDirection == Direction::up || lastDirection == Direction::down) {
+				keypoints.push_back(tmppos);
+			}
+			lastDirection = Direction::left;
 		}
 
 		//˜g•`‰æ
-		DrawBox(offset.x-1, offset.y-1, offset.x + 451, offset.y + 401, 0xffffff, false);
-		DrawBox(offset.x, offset.y, offset.x + 450, offset.y+400 , 0xffffff, false);
+		DxLib::DrawBox(offset.x-1, offset.y-1, offset.x + 451, offset.y + 401, 0xffffff, false);
+		DxLib::DrawBox(offset.x, offset.y, offset.x + 450, offset.y+400 , 0xffffff, false);
 		
 
-		SetDrawScreen(area);
+		DxLib::SetDrawScreen(area);
 		auto tmpPos = playerPos - Vector2(play_areaX, play_areaY);
 		//‹OÕ‰¼•`‰æ
-		DrawBox(tmpPos.x - 1, tmpPos.y - 1, tmpPos.x + 1, tmpPos.y + 1, 0xffffff, true);
+		DxLib::DrawBox(tmpPos.x - 1, tmpPos.y - 1, tmpPos.x + 1, tmpPos.y + 1, 0xffffff, true);
 		/*DrawCircle(playerPos.x- play_areaX, playerPos.y- play_areaY, 1, 0xffffff, true);*/
-		SetDrawScreen(DX_SCREEN_BACK);
+		DxLib::SetDrawScreen(DX_SCREEN_BACK);
 		//‹OÕ•`‰æ
-		DrawGraph(play_areaX, play_areaY, area, true);
+		DxLib::DrawGraph(play_areaX, play_areaY, area, true);
 		//ŽžŠú•`‰æ
-		DrawCircle(playerPos.x, playerPos.y, 3, 0xffff88, true);
-		ScreenFlip();
+		DxLib::DrawCircle(playerPos.x, playerPos.y, 3, 0xffff88, true);
+		DxLib::ScreenFlip();
 	}
 
 	DxLib_End();
